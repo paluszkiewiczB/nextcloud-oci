d023ab958e09edb9746ec638ab4d0517bddf8e42
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-07-02 20:00:13 +0200

    add required oci provider configured through variables

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,25 @@
 terraform {
   required_version = "1.2.4"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "4.82.0"
+    }
+  }
 }
 
-provider "oci" {}
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" {}
+
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  region = var.region
+}
 
