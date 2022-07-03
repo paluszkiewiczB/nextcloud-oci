e807860f099fe8c5ba41585f048448251723eb64
Bartosz Paluszkiewicz <9494671+paluszkiewiczB@users.noreply.github.com>
2022-07-03 15:57:59 +0200

    use private key set as plain string with '\n' instead of EOFs; add lock file

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -31,10 +31,10 @@ variable "region" {
 }
 
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key
-  region           = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key
+  region       = var.region
 }
 
